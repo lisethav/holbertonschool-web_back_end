3fa2f4853608246bf0ee8a71265f05295c894629
lisethav <lisethav@gmail.com>
2022-09-20 23:41:45 -0500

    pep8

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,7 +6,7 @@ from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+async def wait_n(n: int, max_delay: int=10) -> List[float]:
     """return the delay values Parameters"""
     spawn_list = []
     sorted_delays = []
@@ -17,5 +17,4 @@ async def wait_n(n: int, max_delay: int = 10) -> List[float]:
     for future in asyncio.as_completed(spawn_list):
         res = await future
         sorted_delays.append(res)
-
     return sorted_delays
