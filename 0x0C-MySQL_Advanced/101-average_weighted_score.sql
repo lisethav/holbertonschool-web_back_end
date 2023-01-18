4243566b177e810afa1c52df5dbba677f59dd3af
lisethav <lisethav@gmail.com>
2023-01-17 20:39:25 -0500

    all

---
+++ 0x0C-MySQL_Advanced/101-average_weighted_score.sql
@@ -0,0 +1,19 @@
+-- sql languaje holbsc
+delimiter |
+CREATE PROCEDURE ComputeAverageWeightedScoreForUsers() BEGIN
+UPDATE
+	users
+SET
+	average_score = (
+		SELECT
+			sum(p.weight * c.score) / sum(p.weight)
+		FROM
+			projects AS p
+			INNER JOIN corrections AS c ON p.id = c.project_id
+		WHERE
+			c.user_id = users.id
+	);
+
+END;
+|
+/ /
---
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -0,0 +1,10 @@
+-- sql languaje holbsc
+SELECT
+	DISTINCT `band_name`,
+	IFNULL(`split`, 2020) - `formed` as `lifespan`
+FROM
+	`metal_bands`
+WHERE
+	FIND_IN_SET('Glam rock', style)
+ORDER BY
+	`lifespan` DESC;
